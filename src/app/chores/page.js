331e87ef113bbf5eded01593c41dcc2ed48646d5
Raphael Lim <raphaellim@Raphaels-Mac-mini.local>
2024-06-22 17:06:41 +0800

    feat: changed project to next.js

---
+++ src/app/chores/page.js
@@ -0,0 +1,8 @@
+export default function Chores() {
+    return (
+      <div>
+        <h1>Hello this is chores page</h1>
+      </div>
+    );
+  }
+  
---
+++ src/app/leaderboard/page.js
@@ -0,0 +1,8 @@
+export default function Leaderboard() {
+    return (
+      <div>
+        <h1>Hello this is leaderboard page</h1>
+      </div>
+    );
+  }
+  
