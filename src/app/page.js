fd0cc48c485ced00fa333229aabf131eb57aae42
Raphael Lim <raphaellim@Raphaels-Mac-mini.local>
2024-06-22 18:01:16 +0800

    feat: database queries

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
-export default function Home() {
+import { createClient } from '../../utils/supabase/server';
+
+export default async function Home() {
+  const supabase = createClient();
+  const { data: users, error } = await supabase.from("profiles").select('*');
+  console.log(users)
   return (
     <div>
-      <h1>Hello this is home page</h1>
+      <h1>{JSON.stringify(users, null, 2)}</h1>
     </div>
   );
 }
